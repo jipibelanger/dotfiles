fdfb71e90f80070a0507260fb4aaa2ac815aaad6
jp <jipibelanger@users.noreply.github.com>
2020-05-30 17:57:54 -0500

    moved back to apt-get on ubuntu away from homebrew

--- scripts/base_setup.sh
+++ scripts/base_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [[ "${OSTYPE}" != "linux-gnu"* && "${OSTYPE}" != "darwin"* ]]; then
-  echo "${OSTYPE} not supported. Aborting."
-  exit 1
-fi
+
+
+UBUNTU_PKGS="build-essential zsh tmux"
+MACOS_PKGS="gcc zsh tmux"
 
 # setup home directory structure
 command mkdir -pv "${HOME}/workspaces" "${HOME}/.config"
@@ -21,54 +21,24 @@ if [[ ! -f "${HOME}/.ssh/id_rsa" ]]; then
 fi
 
 # install base packages
-echo "Installing base dependencies"
+echo "Installing base packages"
 if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
   echo "Installing for ${OSTYPE}"
   sudo apt-get update && sudo apt-get upgrade
-  sudo apt-get install -y --no-install-recommends \
-    build-essential curl file git zsh
+  sudo apt-get install -y --no-install-recommends "${UBUNTU_PKGS}"
 elif [[ "${OSTYPE}" == "darwin"* ]]; then
   echo "Installing for ${OSTYPE}"
   xcode-select --install
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
+  brew install "${MACOS_PKGS}"
 fi
 
-# ZSH setup
+# make ZSH the default shell
 if [[ -x "$(command -v zsh)" ]]; then
   echo "Setting up ZSH"
   if [[ "${SHELL}" != *"zsh" ]]; then
     chsh -s $(command -v zsh);
   fi
-fi
-
-# install Homebrew
-if [[ ! -x "$(command -v brew)" ]]; then
-  echo "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
-    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
-    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
-  fi
-fi
-
-# install packages, stop if Homebrew is not found
-if [[ ! -x "$(command -v brew)" ]]; then
-  echo "Homebrew not found. Aborting."
-  exit 1
-fi
-
-# install packages
-echo "Installing packages"
-brew update
-brew install gcc tmux
-
-# setup editor
-echo "Installing neovim"
-brew install neovim
-if [[ ! -d "${HOME}/.cache/dein/" ]]; then
-  echo "Installing dein.vim plugin manager"
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "${HOME}/dein_installer.sh"
-  sh "${HOME}/dein_installer.sh" "${HOME}/.cache/dein"
-  rm -f "${HOME}/dein_installer.sh"
 fi
